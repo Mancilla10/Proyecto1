f93dd0de19daaa554a99f6c64206ece4350f1cb9
Mancilla <Mancilla@USUARIO1>
2017-12-09 12:11:38 -0600

    Solo se le añadio una ventana con hola mundo.

--- Ejercicio1/src/ejercicio1/Ejercicio1.java
+++ Ejercicio1/src/ejercicio1/Ejercicio1.java
@@ -5,6 +5,8 @@
  */
 package ejercicio1;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Mancilla
@@ -15,7 +17,7 @@ public class Ejercicio1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        JOptionPane.showMessageDialog(null, "Hola Mundo");
     }
     
 }
